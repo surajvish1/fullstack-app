7e4ee5f2730b72599153f29527b4f80ac153b114
surajvish1 <surajvish101@gmail.com>
2025-06-12 14:01:33 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node backend!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0' , () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 
